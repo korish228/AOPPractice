a6833ea0637fa9b1e99bf66e2905c0431edbaea3
korish <boybkorish@gmail.com>
2019-02-19 19:06:03 +0200

    added aspect which work

--- src/main/java/aop/practice/demo/Context.java
+++ src/main/java/aop/practice/demo/Context.java
@@ -1,12 +1,15 @@
 package aop.practice.demo;
 
+import aop.practice.demo.aspect.LoggingAspect;
 import aop.practice.demo.model.Circle;
 import aop.practice.demo.model.Triangle;
 import aop.practice.demo.service.ShapeService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
+@EnableAspectJAutoProxy(proxyTargetClass=true)
 public class Context {
     @Bean
     public Triangle triangle(){
@@ -21,5 +24,9 @@ public class Context {
     @Bean
     public ShapeService service(){
         return new ShapeService( circle(),triangle());
+    }
+    @Bean
+    public LoggingAspect loggingAspect(){
+        return new LoggingAspect();
     }
 }

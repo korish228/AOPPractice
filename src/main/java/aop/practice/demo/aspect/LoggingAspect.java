7c2782d6c5e83b7d20d4466614608ba512417bf8
korish <boybkorish@gmail.com>
2019-02-19 19:16:52 +0200

    add pointcut

--- src/main/java/aop/practice/demo/aspect/LoggingAspect.java
+++ src/main/java/aop/practice/demo/aspect/LoggingAspect.java
@@ -3,12 +3,27 @@ package aop.practice.demo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
 
-    @Before("execution(public String getName())")
-    public void LoggingAdvice(){
+    @Before("allGetters()")
+    public void loggingAdvice(){
         System.out.println("Advice run . Get Method called");
     }
+
+    @Before("allGetters()")
+    public void secondLoggingAdvice(){
+        System.out.println("Advice run . Get Method called");
+    }
+
+    @Pointcut("execution(* get*(..))")
+    public void allGetters(){}
+
 }
+//@Before("execution(public * aop.practice.demo.model.Circle.*get*(..))")
+//@Before("execution(public * get*(..))")
+//@Before("execution(public * get*(*))")
+//@Before("execution(public * get*())")
+//@Before("execution(public String getName())")

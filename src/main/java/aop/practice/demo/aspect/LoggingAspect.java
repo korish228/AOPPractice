7ac698783fbc6b8a0b94b44a2ede51e3417fea53
korish <boybkorish@gmail.com>
2019-02-21 16:15:33 +0200

    more pointcut expressions

--- src/main/java/aop/practice/demo/aspect/LoggingAspect.java
+++ src/main/java/aop/practice/demo/aspect/LoggingAspect.java
@@ -8,20 +8,25 @@ import org.aspectj.lang.annotation.Pointcut;
 @Aspect
 public class LoggingAspect {
 
-    @Before("allGetters()")
+    @Before("allCircleMethods() && allGetters()")
     public void loggingAdvice(){
         System.out.println("Advice run . Get Method called");
     }
 
     @Before("allGetters()")
     public void secondLoggingAdvice(){
-        System.out.println("Advice run . Get Method called");
+        System.out.println("Second Advice run");
     }
 
     @Pointcut("execution(* get*(..))")
     public void allGetters(){}
 
+    @Pointcut("within(aop.practice.demo.model.Circle)")
+    public void allCircleMethods(){}
+
+
 }
+//@Pointcut("within(aop.practice.demo.model..*)")
 //@Before("execution(public * aop.practice.demo.model.Circle.*get*(..))")
 //@Before("execution(public * get*(..))")
 //@Before("execution(public * get*(*))")
